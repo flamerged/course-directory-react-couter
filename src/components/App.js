3bdf9ac96943397247ba934408000c8bd5afe499
flamerged <mersad.ajanovic@googlemail.com>
2020-03-11 12:20:19 +0100

    Added Routing to sub components

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+// Components
+import Home from './Home';
+import About from './About';
+import Header from './Header';
+import Courses from './Courses';
+import Teachers from './Teachers';
 
 const App = () => (
-  <div className="container">
-
-  </div>
+    <BrowserRouter>
+        <div className="container">
+            <Header />
+            <Route exact path="/" component={Home} />
+            {/*You use render when you want to pass on props*/}
+            <Route path="/about" render={() => <About title="About" />} />
+            <Route path="/teachers" component={Teachers} />
+            <Route path="/courses" component={Courses} />
+        </div>
+    </BrowserRouter>
 );
 
 export default App;
